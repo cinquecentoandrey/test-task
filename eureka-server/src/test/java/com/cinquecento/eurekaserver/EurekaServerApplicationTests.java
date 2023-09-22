2a311b32494d8825707fc4685cb487bf782825c6
Andrey <chehonte16@gmail.com>
2023-09-22 19:17:22 +0300

    Initial commit

---
+++ eureka-server/src/test/java/com/cinquecento/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinquecento.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
